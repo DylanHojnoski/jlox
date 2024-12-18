28e249c1166e11887947b79848bc8b4b5e7406f1
Dylan Hojnoski <dylanhoj@gmail.com>
2024-12-18 14:53:18 -0500

    Added modulo operator %

--- src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -115,6 +115,9 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
             case STAR:
                 checkNumberOperands(expr.operator, left, right);
                 return (double)left * (double)right;
+            case PERCENT:
+                checkNumberOperands(expr.operator, left, right);
+                return (double)left % (double)right;
         }
 
         // Unreachabe
--- src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -73,6 +73,7 @@ class Scanner {
             case '+': addToken(PLUS); break;
             case ';': addToken(SEMICOLON); break;
             case '*': addToken(STAR); break;
+            case '%': addToken(PERCENT); break;
             case '!':
                 addToken(match('=') ? BANG_EQUAL : BANG);
                 break;
